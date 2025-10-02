b8279e2f0039c4c5d942b99441ca483cf848b64c
Raulms29 <UO287827@uniovi.es>
2025-10-02 13:24:12 +0200

    test: ✅ Added more editing tests

--- test/e2e/editing.e2e.ts
+++ test/e2e/editing.e2e.ts
@@ -14,22 +14,70 @@ describe('Editor Testing', () => {
         await browser.reloadSession();
     });
 
-    // TODO Test all the editor features and extensions
 
-    it('Should properly write on a Note', async () => {
-        await POEditor.write('# This is a test note.', 'h1', "This is a test note.");
+    describe('Basic Editor Actions', () => {
+        it('Should properly write a heading on a Note', async () => {
+            await POEditor.write('# This is a test note.\n', 'h1', 'This is a test note.');
+        });
+
+        it('Should properly undo and redo writing on a Note', async () => {
+            await POEditor.write('This is a test note.');
+            await POEditor.undo();
+            await expect((await POEditor.getEditor()).$(`p=This is a test note.`)).not.toBeExisting();
+            await POEditor.redo();
+            await expect((await POEditor.getEditor()).$(`p=This is a test note.`)).toBeExisting();
+        });
+
+        it('Should properly activate and deactivate focusMode', async () => {
+            await PONoteSpace.activateFocusMode();
+            await PONoteSpace.deactivateFocusMode();
+        });
     });
 
-    it('Should properly undo and redo writing on a Note', async () => {
-        await POEditor.write('This is a test note.');
-        await POEditor.undo();
-        await expect((await POEditor.getEditor()).$(`p=This is a test note.`)).not.toBeExisting();
-        await POEditor.redo();
-        await expect((await POEditor.getEditor()).$(`p=This is a test note.`)).toBeExisting();
+    describe('Formatting', () => {
+        it('Should apply bold using markdown', async () => {
+            await POEditor.writeBold('Bold Test');
+        });
+
+        it('Should apply italic using markdown', async () => {
+            await POEditor.writeItalic('Italic Test');
+        });
+
+        // TODO must be done viua BubbleMenu
+        // it('Should apply underline using bubble menu button', async () => {
+        //     await POEditor.writeUnderline('Underline Test');
+        // });
+
+        it('Should apply strike using bubble menu button', async () => {
+            await POEditor.writeStrike('Strike Test');
+        });
     });
 
-    it('Should properly activate and deactivate focusMode', async () => {
-        await PONoteSpace.activateFocusMode();
-        await PONoteSpace.deactivateFocusMode();
+    describe('Block Elements', () => {
+        it('Should create blockquote using markdown', async () => {
+            await POEditor.writeBlockquote('A quoted line');
+        });
+
+        it('Should create bullet list using markdown', async () => {
+            await POEditor.writeBulletList(['Item 1', 'Item 2', 'Item 3', 'Item 4']);
+        });
+
+        it('Should create ordered list using markdown', async () => {
+            await POEditor.writeOrderedList(['First', 'Second', 'Third', 'Fourth']);
+        });
+
+        it('Should create code block using markdown', async () => {
+            await POEditor.writeCodeBlock('console.log("hi");', 'js');
+        });
+
+        it('Should create task list using markdown', async () => {
+            await POEditor.writeTaskList(['Task A', 'Task B']);
+        });
+    });
+
+    describe('Links', () => {
+        it('Should create links using markdown', async () => {
+            await POEditor.writeLink('My Link', 'https://example.com');
+        });
     });
 });
--- test/e2e/pageobjects/PO_Editor.ts
+++ test/e2e/pageobjects/PO_Editor.ts
@@ -26,5 +26,117 @@ export default class POEditor extends POApp {
         await browser.keys(['Control', 'y']);
     }
 
+    static async writeBold(text: string) {
+        const editor = await POEditor.getEditor();
+        await editor.click();
+        await browser.keys(`**${text}**`);
+        await expect(editor.$('strong')).toBeExisting();
+        await expect(editor.$('strong')).toHaveText(text);
+    }
+
+    static async writeItalic(text: string) {
+        const editor = await POEditor.getEditor();
+        await editor.click();
+        await browser.keys(`*${text}*`);
+        await expect(editor.$('em')).toBeExisting();
+        await expect(editor.$('em')).toHaveText(text);
+    }
+
+    static async writeStrike(text: string) {
+        const editor = await POEditor.getEditor();
+        await editor.click();
+        await browser.keys(`~~${text}~~`);
+        await expect(editor.$('s')).toBeExisting();
+        await expect(editor.$('s')).toHaveText(text);
+    }
+
+    static async writeBlockquote(text: string) {
+        const editor = await POEditor.getEditor();
+        await editor.click();
+        await browser.keys(`> ${text}`);
+        await browser.keys(['Enter']);
+        await expect(editor.$('blockquote')).toBeExisting();
+        await expect(editor.$('blockquote')).toHaveText(text);
+    }
+
+    static async writeBulletList(items: string[]) {
+        const editor = await POEditor.getEditor();
+        await editor.click();
+        for (let i = 0; i < items.length; i++) {
+            if (i === 0) {
+                await browser.keys(`- ${items[i]}`);
+            } else {
+                await browser.keys(`${items[i]}`);
+            }
+            if (i < items.length - 1) {
+                await browser.keys(['Enter']);
+            }
+        }
+        const listItems = editor.$$('ul>li');
+        await expect(listItems).toBeElementsArrayOfSize(items.length);
+        for (let i = 0; i < items.length; i++) {
+            await expect(listItems[i]).toHaveText(items[i]);
+        }
+    }
+
+    static async writeOrderedList(items: string[]) {
+        const editor = await POEditor.getEditor();
+        await editor.click();
+        for (let i = 0; i < items.length; i++) {
+            if (i === 0) {
+                await browser.keys(`1. ${items[i]}`);
+            } else {
+                await browser.keys(`${items[i]}`);
+            }
+            if (i < items.length - 1) {
+                await browser.keys(['Enter']);
+            }
+        }
+        const listItems = editor.$$('ol>li');
+        await expect(listItems).toBeElementsArrayOfSize(items.length);
+        for (let i = 0; i < items.length; i++) {
+            await expect(listItems[i]).toHaveText(items[i]);
+        }
+    }
+
+    static async writeCodeBlock(code: string, lang = 'js') {
+        const editor = await POEditor.getEditor();
+        await editor.click();
+        await browser.keys('```' + lang);
+        await browser.keys(['Enter']);
+        await browser.keys(code);
+        const codeBlock = editor.$('pre code');
+        await expect(codeBlock).toBeExisting();
+        await expect(codeBlock).toHaveText(code);
+    }
+
+    static async writeTaskList(items: string[]) {
+        const editor = await POEditor.getEditor();
+        await editor.click();
+        for (let i = 0; i < items.length; i++) {
+            if (i === 0) {
+                await browser.keys(`[] ${items[i]}`);
+            } else {
+                await browser.keys(`${items[i]}`);
+            }
+            if (i < items.length - 1) {
+                await browser.keys(['Enter']);
+            }
+        }
+        const checkboxes = editor.$$('input[type="checkbox"]');
+        await expect(checkboxes).toBeElementsArrayOfSize(items.length);
+    }
+
+    static async writeLink(text: string, url: string) {
+        const editor = await POEditor.getEditor();
+        await editor.click();
+        await browser.keys(`[${text}](${url})`);
+        await browser.keys(['Enter']);
+        const link = editor.$('a');
+        await expect(link).toBeExisting();
+        await expect(link).toHaveText(text);
+        await expect(link).toHaveAttribute('href', url);
+    }
+
 
 }
